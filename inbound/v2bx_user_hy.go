1e4ea986d061608cbec9549992116388c17f35f4
wyx2685 <wyx2685@gmail.com>
2024-01-28 00:47:03 +0900

    Fix hysteria users update

--- inbound/v2bx_user_hy.go
+++ inbound/v2bx_user_hy.go
@@ -7,50 +7,37 @@ import (
 )
 
 func (h *Hysteria) AddUsers(users []option.HysteriaUser) error {
-	pws := make([]string, len(users)+len(h.userNameList))
-	for i := range users {
-		pws[i] = users[i].AuthString
+	for _, user := range users {
+		h.userNameList = append(h.userNameList, user.AuthString)
 	}
-	if cap(h.userNameList)-len(h.userNameList) >= len(users) {
-		h.userNameList = append(h.userNameList, pws...)
-	} else {
-		tmp := make([]string, 0, len(h.userNameList)+len(users)+10)
-		tmp = append(tmp, h.userNameList...)
-		tmp = append(tmp, pws...)
-		h.userNameList = tmp
-	}
+
 	indexs := make([]int, len(h.userNameList))
 	for i := range h.userNameList {
 		indexs[i] = i
 	}
+
 	h.service.UpdateUsers(indexs, h.userNameList)
 	return nil
 }
 
-func (h *Hysteria) DelUsers(name []string) error {
-	if len(name) == 0 {
+func (h *Hysteria) DelUsers(names []string) error {
+	if len(names) == 0 {
 		return nil
 	}
-	is := make([]int, 0, len(name))
-	ulen := len(name)
-	for i := range h.userNameList {
-		for _, u := range name {
-			if h.userNameList[i] == u {
-				is = append(is, i)
-				ulen--
-			}
-			if ulen == 0 {
-				break
-			}
+
+	toDelete := make(map[string]struct{})
+	for _, name := range names {
+		toDelete[name] = struct{}{}
+	}
+
+	remaining := make([]string, 0, len(h.userNameList))
+	for _, user := range h.userNameList {
+		if _, found := toDelete[user]; !found {
+			remaining = append(remaining, user)
 		}
 	}
-	ulen = len(h.userNameList)
-	for _, i := range is {
-		h.userNameList[i] = h.userNameList[ulen-1]
-		h.userNameList[ulen-1] = ""
-		h.userNameList = h.userNameList[:ulen-1]
-		ulen--
-	}
+
+	h.userNameList = remaining
 	indexs := make([]int, len(h.userNameList))
 	for i := range h.userNameList {
 		indexs[i] = i
@@ -60,48 +47,37 @@ func (h *Hysteria) DelUsers(name []string) error {
 }
 
 func (h *Hysteria2) AddUsers(users []option.Hysteria2User) error {
-	indexs := make([]int, len(users)+len(h.userNameList))
-	pws := make([]string, len(users)+len(h.userNameList))
-	for i := range users {
+	for _, user := range users {
+		h.userNameList = append(h.userNameList, user.Password)
+	}
+
+	indexs := make([]int, len(h.userNameList))
+	for i := range h.userNameList {
 		indexs[i] = i
-		pws[i] = users[i].Password
 	}
-	if cap(h.userNameList)-len(h.userNameList) >= len(users) {
-		h.userNameList = append(h.userNameList, pws...)
-	} else {
-		tmp := make([]string, 0, len(h.userNameList)+len(users)+10)
-		tmp = append(tmp, h.userNameList...)
-		tmp = append(tmp, pws...)
-		h.userNameList = tmp
-	}
+
 	h.service.UpdateUsers(indexs, h.userNameList)
 	return nil
 }
 
-func (h *Hysteria2) DelUsers(name []string) error {
-	if len(name) == 0 {
+func (h *Hysteria2) DelUsers(names []string) error {
+	if len(names) == 0 {
 		return nil
 	}
-	is := make([]int, 0, len(name))
-	ulen := len(name)
-	for i := range h.userNameList {
-		for _, u := range name {
-			if h.userNameList[i] == u {
-				is = append(is, i)
-				ulen--
-			}
-			if ulen == 0 {
-				break
-			}
+
+	toDelete := make(map[string]struct{})
+	for _, name := range names {
+		toDelete[name] = struct{}{}
+	}
+
+	remaining := make([]string, 0, len(h.userNameList))
+	for _, user := range h.userNameList {
+		if _, found := toDelete[user]; !found {
+			remaining = append(remaining, user)
 		}
 	}
-	ulen = len(h.userNameList)
-	for _, i := range is {
-		h.userNameList[i] = h.userNameList[ulen-1]
-		h.userNameList[ulen-1] = ""
-		h.userNameList = h.userNameList[:ulen-1]
-		ulen--
-	}
+
+	h.userNameList = remaining
 	indexs := make([]int, len(h.userNameList))
 	for i := range h.userNameList {
 		indexs[i] = i
